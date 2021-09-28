b2205729a7fa5aa1db0f2b4d91d375c697789d3e
KashinaShurlakova <vika24sh@gmail.com>
2021-09-28 16:48:23 +0700

    Lab_2_KashinaShurlakova

---
+++ Lab_2_KSh.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class HelloWorld{
+
+     public static void main(String []args){
+        Scanner input = new Scanner(System.in);
+         int A=12;
+         String B= "KashinaShurlakova";
+         String C= input.nextLine(); 
+        System.out.println("Hello World");
+        System.out.println(A); 
+        System.out.println(B);
+        System.out.println(C);
+     }
+}
